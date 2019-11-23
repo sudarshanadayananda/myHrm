39462ede5acea88169cbe6179c5a0ff597852266
Sammani Wimalaratne <umangasam16@gmail.com>
2019-11-24 00:00:38 +0530

    addUser updated

--- frontend/src/app/modules/admin-user/add-user/add-user.component.ts
+++ frontend/src/app/modules/admin-user/add-user/add-user.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 import { UserService } from '../../../core/services/user/user.service';
 import { AuthService } from '../../../core/services/auth/auth.service';
 
@@ -11,27 +11,37 @@ import { AuthService } from '../../../core/services/auth/auth.service';
   styleUrls: ['./add-user.component.scss']
 })
 export class AddUserComponent implements OnInit {
-
+  addUserForm: FormGroup;
   name: string;
   email: string;
   password: string;
   role: string;
 
-   options = [
-    { name: "ADMIN_USER", value: 1 },
-    { name: "APP_USER", value: 2 }
-   ];
+  //  options = [
+  //   { name: "ADMIN_USER", value: 1 },
+  //   { name: "APP_USER", value: 2 }
+  //  ];
 
-   AddUserForm = new FormGroup({
-     name: new FormControl(''),
-     email: new FormControl(''),
-     password:new FormControl(''),
-     role:new FormControl(''),
-   });
+  options = ['ADMIN_USER', 'APP_USER'];
 
-  constructor(private authService: AuthService, private userService: UserService, private router: Router) { }
+  
+  //  this.loginForm = new FormGroup({
+  //   email: new FormControl(null, { validators: [Validators.required] }),
+  //   password: new FormControl(null, { validators: [Validators.required] }),
+  // });
+  constructor(private authService: AuthService, private userService: UserService, private router: Router) {
+   this.addUserForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      password: new FormControl(''),
+      role: new FormControl(''),
+    });
+   }
 
   ngOnInit() {
+    // this.AddUserForm = this.formBuilder.group({
+    //   role: ['ADMIN_USER']
+    // });
   }
 
   addUser(AddUserForm): void {
